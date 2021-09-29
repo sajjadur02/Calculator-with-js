89d41be1dcba83db30b12090b46a3c195c9dbb52
Sajjadur Rahman <sajjadur28182@gmail.com>
2021-09-29 16:03:20 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calculator-with-js
+ simple calculator design with html, css and javascript
---
+++ index.js
@@ -0,0 +1,5 @@
+
+function calculate(number){
+    document.getElementById("result").value=document.getElementById("result").value + number;
+}
+
